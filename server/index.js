571c8e5da1be65856ea243d9eb6ded13ea6a3936
suhail_yousaf <ysuhail444@gmail.com>
2023-10-06 21:49:19 +0530

    commit

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.get('/',(req, res)=>{
 })
 
 MONGO_URI ='mongodb://0.0.0.0:27017/StockTracker'
-mongoose.connect(MONGO_URI)
+mongoose.connect(MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true }  )
   .then(() => console.log('Connected Database!'))
   .catch((error)=>{
     console.log(`mongoDb Not connected ${error}`)
